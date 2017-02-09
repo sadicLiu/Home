e35d04be8fb095d2bcb80a1393954de23a455864
liuhy <190978752@qq.com>
2017-02-09 22:16:31 +0800

    将angular添加到项目中

--- app/app.js
+++ app/app.js
@@ -1,18 +1,22 @@
+(function (angular) {
+	'use strict';
 
-'use strict';
+	var myShare = angular.module('myShare', ['ngRoute', 'myShare.homeModule']);
 
-(function () {
+	myShare.config(['$routeProvider', function ($routeProvider) {
+		$routeProvider.otherwise({redirectTo: '/in_theaters/1'});
+	}]);
 
 	/*
 	 * 为每个category注册事件
 	 * */
-	$.each($(".category"), function (index, element) {
-		let $category = $(element);
-		let $box = $category.children('.box');
-		$category.on('mouseenter', function () {
-			$box.addClass('animated pulse');
-		}).on('mouseleave', function () {
-			$box.removeClass('animated pulse');
-		});
-	});
-})();
+	/*$.each($(".category"), function (index, element) {
+	 var $category = $(element);
+	 var $box = $category.children('.box');
+	 $category.on('mouseenter', function () {
+	 $box.addClass('animated pulse');
+	 }).on('mouseleave', function () {
+	 $box.removeClass('animated pulse');
+	 });
+	 });*/
+})(angular);
